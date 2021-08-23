ce90f11adaac0a05f5147c6e000d4d3ad50db07c
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2021-08-23 08:57:44 +0200

    MOOC-2362 Feat: replace typo from vvt to vtt

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='audioplayer-xblock',
-    version='0.1.2',
+    version='0.1.4',
     description='Navoica Audio Player XBlock',
     license='UNKNOWN',
     packages=[
